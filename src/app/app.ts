bedc3740579e02fb85a76418f7c1c08a37c9a75d
Olga Macias <olgamacara@gmail.com>
2025-08-13 13:23:57 +0200

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmpleadosComponent } from './empleados/empleados.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({ /* Decorador que tiene definido un 
              selector, una plantilla html y un archivo css */
   selector: 'app-root',
-  imports: [RouterOutlet, EmpleadosComponent], /* Importar el componente EmpleadosComponent 
+  imports: [RouterOutlet, EmpleadosComponent, FormsModule], /* Importar el componente EmpleadosComponent 
                                                para poder utilizarlo /visualizarlo dentro de 
                                                la página principal */
   templateUrl: './app.html',
--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-empleado',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './empleado.html',
   //  template: `<p style="text-align: center;">Empleado</p>`,
   styleUrl: './empleado.css'
@@ -10,10 +11,35 @@ import { Component } from '@angular/core';
 })
 export class Empleado {
 
+
   nombre = "Juan"
   apellido = "Perez"
   edad = 18
-  // empresa = "Angular"
+  empresa = "Telepizza"
+
+
+  // propiedades binding
+  habilitarCuadroProperty = false
+  userRegistradoProperty = false
+  textoRegistradoProperty = "No hay nadie registrado"
+
+  getRegistroUsuario() {
+    this.userRegistradoProperty = false
+  }
+
+  // función para crear la ventana emergente
+  setUsuarioRegistrado(nombreEvento: Event) {
+    // alert("Usuario registrado")
+    // this.textoRegistradoProperty = "El usuario se ha registrado correctamente"
+    //alert(nombreEvento.target)
+
+    /* casting de Evento a HTMLInputElement */
+    if ((<HTMLInputElement>nombreEvento.target).value == "si") {
+      this.textoRegistradoProperty = "El usuario se ha registrado correctamente"
+    } else {
+      this.textoRegistradoProperty = "No hay nadie registrado"
+    }
+  }
 
   /** para hacerlo con botón: 
   guardarEmpresa(value: string) {
